c42f249746a80c9e3afcc85bc88cab8cf1384d59
msuresu <suresu.manivannan@one.verizon.com>
2015-11-01 19:50:01 +0530

    Update Global.asax.cs
    asd

--- VZ.RestService/VZ.RestService/Global.asax.cs
+++ VZ.RestService/VZ.RestService/Global.asax.cs
@@ -26,14 +26,14 @@ namespace VZ.RestService
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-          //  if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
-           // {
-               HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+           if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+               HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE");
 
                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
                 HttpContext.Current.Response.End();
-           // }
+            }
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
